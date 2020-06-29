6d34f7b2c0f8170be09b44c8e26cc2a4be46a4d5
Galactic-Cosmonaut <kevin.miner.cs@gmail.com>
2020-06-28 22:15:05 -0600

    History files finished

--- src/README.md
+++ src/README.md
@@ -11,11 +11,16 @@ When runnig it will prompt for input with `> `.
 Type any string and hit enter. This will print your string parsed as tokens  
 along with their position.
 
-Program will run indefinitely, to stop it use `Ctrl-c`
-
 Memory allocation was required to acheive this.
 To free this memory, uncomment `free_tokens(tokens)` and `print_tokens(tokens)`
 in main.c.
 This will show what is left after we free the memory.
 
-To clean unecessary files run `make clean`
+# History:
+
+Stores input on a linked list which can be retrieved by inputing `!#`
+where `#` is the id or position of the string to retrieve.
+
+### Notes:
+- To clean unecessary files run `make clean`.
+- Program will run indefinitely, to stop it use `Ctrl-c` or enter `0`.
--- src/history.c
+++ src/history.c
@@ -1,1 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "history.h"
+
+/* Start root node with default values */
+List *init_history() {
+  List *list = (List *)malloc(sizeof(List));
+  Item *item = (Item *)malloc(sizeof(Item));
+  list->root = item;
+
+  item->id = 0;
+  item->str = "--Root--\n";
+  item->next = NULL;
+  return list;
+}
+
+/* Add a new node to the list */
+void add_history(List *list, char *str) {
+ 
+  Item *curr = list->root;
+  while (curr->next != NULL) {
+    curr = curr->next;
+  }
+  int i = curr->id;
+  curr->next = (Item *) malloc(sizeof(Item));
+  curr->next->id = i+1;
+  curr->next->str = str;
+  curr->next->next = NULL;
+}
+
+/* Retrieve a specific item on the list */
+char *get_history(List *list, int id) {
+  if (list == 0)
+    return NULL;
+  
+  Item *p = list->root;
+  while (p->id != id) {
+    p = p->next;
+  }
+  printf("%s", p->str);
+  return p->str;
+}
+
+/* Print list */
+void print_history(List *list) {
+  Item *p = list->root;
+  while (p != NULL) {
+    printf("%s", p->str);
+    p = p->next;
+  }
+}
+
+/* Free memory for list */
+void free_history(List *list) {
+  Item *p = list->root;
+  Item *curr;
+  while (p != NULL) {
+    curr = p;        //Temporal holder so not to loose position
+    free(p);
+    p = p->next;      
+  }
+  free(list);
+}
+
